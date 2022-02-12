ef6a11290fc7bba01b1465eceaa4f2742916e5f2
tnhuhoang1 <1851061587@e.tlu.edu.vn>
2022-02-12 19:36:01 +0700

    update base dao

--- tnhlibrary/src/main/java/com/tnh/tnhlibrary/room/BaseDao.kt
+++ tnhlibrary/src/main/java/com/tnh/tnhlibrary/room/BaseDao.kt
@@ -6,15 +6,30 @@ import androidx.room.*
 interface BaseDao<MODEL>{
     /**
      * insert one object into table in the database
+     *
+     * If the object already exists in table, place it
+     *
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOne(data: MODEL)
 
     /**
-     * insert one or more objects into table in the database
+     * insert one or more objects into table in the database, replace if exists
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(vararg data: MODEL)
+
+    /**
+     * insert on object into table if it is not exists
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertOneIfNotExists(data: MODEL)
+
+    /**
+     * insert one or more objects into table if it is not exists
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAllIfNotExists(data: MODEL)
 
     /**
      * delete one object in database
@@ -23,6 +38,14 @@ interface BaseDao<MODEL>{
      */
     @Delete
     suspend fun deleteOne(data: MODEL): Int
+
+    /**
+     * delete one or more objects in database
+     *
+     * @return the number of rows that were deleted
+     */
+    @Delete
+    suspend fun deleteAll(vararg data: MODEL): Int
 
     /**
      * update one object in database
