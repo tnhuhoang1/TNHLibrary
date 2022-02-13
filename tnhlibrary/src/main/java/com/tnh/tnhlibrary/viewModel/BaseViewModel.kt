271bfd477a46c000696f201644376a1faf6ef809
tnhuhoang1 <1851061587@e.tlu.edu.vn>
2022-02-13 14:13:16 +0700

    add base view model

---
+++ tnhlibrary/src/main/java/com/tnh/tnhlibrary/viewModel/BaseViewModel.kt
@@ -0,0 +1,22 @@
+package com.tnh.tnhlibrary.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tnh.tnhlibrary.liveData.EventLiveData
+import com.tnh.tnhlibrary.liveData.utils.toLiveData
+
+open class BaseViewModel: ViewModel() {
+    private val _message = MutableLiveData<EventLiveData<String>>()
+    val message = _message.toLiveData()
+
+    fun setMessage(message: String){
+        _message.postValue(EventLiveData(message))
+    }
+
+    private val _clickEvent = MutableLiveData<EventLiveData<String>>()
+    val clickEvent = _clickEvent.toLiveData()
+
+    fun dispatchClickEvent(eventName: String){
+        _clickEvent.postValue(EventLiveData(eventName))
+    }
+}
