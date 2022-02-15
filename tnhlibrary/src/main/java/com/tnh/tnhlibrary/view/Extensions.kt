6630981879300af3b2e82d62768c51ecbeaa60ae
tnhuhoang1 <1851061587@e.tlu.edu.vn>
2022-02-15 08:48:27 +0700

    add view extensions

---
+++ tnhlibrary/src/main/java/com/tnh/tnhlibrary/view/Extensions.kt
@@ -0,0 +1,69 @@
+package com.tnh.tnhlibrary.view
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.tnh.tnhlibrary.trace
+
+fun View.gone(){
+    this.visibility = View.GONE
+}
+
+inline fun View.doAndGone(block: View.() -> Unit){
+    block()
+    gone()
+}
+
+fun View.show(){
+    this.visibility = View.VISIBLE
+}
+
+inline fun View.doAndShow(block: View.() -> Unit){
+    block()
+    show()
+}
+
+/**
+ * set visibility of this view to INVISIBLE
+ */
+fun View.hide(){
+    this.visibility = View.INVISIBLE
+}
+
+inline fun View.doAndHide(block: View.() -> Unit){
+    block()
+    hide()
+}
+
+
+/**
+ * execute the block if the view visibility is not set to VISIBLE
+ */
+inline fun View.ifVisible(
+    block: View.() -> Unit
+){
+    if(this.visibility != View.VISIBLE){
+        block()
+    }
+}
+
+/**
+ * execute the block if the view visibility is not set to GONE
+ */
+inline fun View.ifGone(
+    block: View.() -> Unit
+){
+    if(this.visibility != View.GONE){
+        block()
+    }
+}
+
+
+fun View.hideKeyboard(){
+    try {
+        (context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+        .hideSoftInputFromWindow(windowToken, 0)
+    }catch (e: Exception){
+        trace(e)
+    }
+}
---
+++ tnhlibrary/src/main/java/com/tnh/tnhlibrary/view/snackbar/Extensions.kt
@@ -0,0 +1,28 @@
+package com.tnh.tnhlibrary.view.snackbar
+
+import android.app.Activity
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.tnh.tnhlibrary.trace
+
+fun Activity.showSnackbar(
+    message: String, length: Int = Snackbar.LENGTH_SHORT
+){
+    try {
+        Snackbar.make(findViewById(android.R.id.content), message, length).show()
+    }catch (e: Exception){
+        trace(e)
+    }
+}
+
+fun View.showSnackBar(message: String, length: Int = Snackbar.LENGTH_SHORT){
+    Snackbar.make(this, message, length).show()
+}
+
+fun View.buildSnackBar(
+    message: String,
+    length: Int = Snackbar.LENGTH_SHORT,
+    builder: (Snackbar)-> Unit = {}
+){
+    Snackbar.make(this, message, length).apply(builder).show()
+}
